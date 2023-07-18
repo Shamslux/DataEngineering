aeaf42ff0974ad9e9a2d69075286aa5872aa74db
SHAMSLUX-PC\jpmul <jpmamed@gmail.com>
2023-07-17 22:28:56 -0300

    (update): Scripts folder with hive commands in shell

---
+++ Hive_Impala/Scripts/hive_commands.sh
@@ -0,0 +1,49 @@
+# Create the HDFS directory to receive the files for the car rental company ("locacao" is the Portuguese word for car rental).
+hdfs dfs -mkdir /user/cloudera/locacao
+
+# The directory is changed to the Cloudera Downloads folder, where the files downloaded for the project are located. 
+#Files are copied to the directory, namely all files of the CSV type.
+cd /home/cloudera/Downloads
+
+hdfs dfs -put *.csv /user/cloudera/locacao 
+
+# Use Beeline which is a client to access Hive.
+beeline
+
+# Connect to Hive within the Beeline client.
+!connect jdbc:hive2://
+
+# Creating, showing and dropping a test database.
+create database test;
+
+show database test;
+
+drop database test cascade;
+
+# Creating the locacao (rental) database and using it.
+create database locacao;
+
+use locacao;
+
+# Creating the first table in locacao database.
+
+CREATE EXTERNAL TABLE CLIENTESs (
+	idcliente 		    int
+	, cnh			    string
+	, cpf			    string
+	, validadecnh	    date
+	, nome			    string
+	, datacadastro	    date
+	, datanascimento    date
+	, telefone		    string
+	, status		    string)
+
+row format delimited fields terminated by ',' STORED AS TEXTFILE;
+
+# "Inserting data" into the table CLIENTES.
+
+LOAD DATA INPATH '/user/cloudera/locacao/clientes.csv' INTO TABLE CLIENTES;
+
+# Querying against the table CLIENTES.
+
+SELECT * FROM CLIENTES;
