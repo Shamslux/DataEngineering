6000fef19c36c2b0c7a03b656284f10888a15003
Shamslux <jpmamed@gmail.com>
2024-04-07 08:23:07 -0300

    (new): Arithmetic operators Python script

---
+++ DIO-Python-Developer-Formation/scripts/arithmetic_operators.py
@@ -0,0 +1,47 @@
+# Arithmetic operators
+
+# Addition
+print(1 + 1)
+
+# Subtraction
+print(10 - 2)
+
+# Multiplication
+print(4 * 3)
+
+# Division
+print(12 / 3)
+
+# Integer Division
+print(12 // 2)
+
+# Modulus
+print(10 % 3)
+
+# Exponentiation
+print( 2 ** 3)
+
+# Precedence of Arithmetic Operators
+
+print(10 - 5 * 2)
+print((10 - 5) * 2)
+print(10 ** 2 * 2)
+print(10 ** (2 * 2))
+print(10 / 2 * 4)
+#-----------------------------------------------
+
+value_1 = 20
+value_2 = 10
+
+print(value_1 + value_2)
+print(value_1 - value_2)
+print(value_1 / value_2)
+print(value_1 // value_2)
+print(value_1 * value_2)
+print(value_1 % value_2)
+print(value_1 ** value_2)
+
+x = (10 + 5) * 4
+y = (10 / 2) + (25 * 2) - (2 ** 2)
+print(x)
+print(y)
