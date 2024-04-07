3fc323948d9840e11245f490fcfd0398e7d80325
Shamslux <jpmamed@gmail.com>
2024-04-07 11:21:26 -0300

    (new): Comparison Operators

---
+++ DIO-Python-Developer-Formation/scripts/comparison_operators.py
@@ -0,0 +1,19 @@
+# Equality comparison
+balance = 450
+withdrawal = 200
+
+print(balance == withdrawal)
+
+# Inequality comparison
+balance = 450
+withdrawal = 200
+
+print(balance != withdrawal)
+
+# Greater than / Greater than or equal comparison
+print(balance > withdrawal)
+print(balance >= withdrawal)
+
+# Less than / Less than or equal comparison
+print(balance < withdrawal)
+print(balance <= withdrawal)
