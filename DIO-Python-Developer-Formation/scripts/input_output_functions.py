ce6fe5d8e4f8d3bce8464fd8a93f0b99a0a43d64
Shamslux <jpmamed@gmail.com>
2024-04-07 07:45:11 -0300

    (new): Input-Output Python Script + Type Conversions Python Script

---
+++ DIO-Python-Developer-Formation/scripts/input_output_functions.py
@@ -0,0 +1,18 @@
+# Input function
+
+name = input("Write your name: ")
+
+# Print function
+
+name = input("Write your name: ")
+print(name)
+
+# Print function using end and sep
+
+name = input("Write your name: ")
+age = input("Write your age: ")
+
+print(name, age)
+print(name, age, end=" ... \n")
+print(name, age, sep="#", end=" ... \n")
+print(name, age, sep="#")
