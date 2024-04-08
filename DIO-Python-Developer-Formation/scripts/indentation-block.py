6d82df2a8e0ef8fea145a706a9a3cc41c8b0b34a
João Paulo Müller <79280485+Shamslux@users.noreply.github.com>
2024-04-08 13:24:35 -0300

    (new): Indentation and Blocks Python script

---
+++ DIO-Python-Developer-Formation/scripts/indentation-block.py
@@ -0,0 +1,6 @@
+def withdraw(self, value: float) -> None: # Beginning of method block
+    if self.balance >= value: # Beginning of if block
+        self.balance -= value
+    # Ending of if block
+
+# Ending of method block
