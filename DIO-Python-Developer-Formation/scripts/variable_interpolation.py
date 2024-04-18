714573ed0a626dbc405a11662e1601a914e2b6d0
João Paulo Müller <79280485+Shamslux@users.noreply.github.com>
2024-04-18 09:10:52 -0300

    (new): Variable Interpolation Python script

---
+++ DIO-Python-Developer-Formation/scripts/variable_interpolation.py
@@ -0,0 +1,26 @@
+# Old Style %
+
+name = "William"
+age = 28
+job = "Programmer"
+language = "Python"
+
+print("Hello, I am %s. I am %d years old. I work as %s and I use the %s language!" % (name, age, job, language))
+
+# Format() method
+
+name = "William"
+age = 28
+job = "Programmer"
+language = "Python"
+
+print("Hello, I am {0}. I am {1} years old. I work as {2} and I use the {3} language!" .format(name, age, job, language))
+
+# f-string
+
+name = "William"
+age = 28
+job = "Programmer"
+language = "Python"
+
+print(f"Hello, I am {name}. I am {age} years old. I work as {job} and I use the {language} language!")
